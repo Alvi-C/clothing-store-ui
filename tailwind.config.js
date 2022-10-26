dcf7bb7b5901fef3c5ae2ef6b23a3744fd80e365
Alvi Chowdhury <alvic5050@gmail.com>
2022-10-26 16:33:19 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px'
+    },
+
+    container: {
+      center: true,
+      padding: '1rem'
+    },
+
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+        roboto: "'Roboto Slab', serif",
+      }
+    },
+
+  },
+  plugins: [],
+}
