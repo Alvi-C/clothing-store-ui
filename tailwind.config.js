26041ba9ef7a9cdfca96c444ee9703089af584c7
Alvi Chowdhury <alvic5050@gmail.com>
2022-10-26 17:56:25 +0600

    catalog page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px'
-    },
+    // screens: {
+    //   sm: '640px',
+    //   md: '768px',
+    //   lg: '1024px',
+    //   xl: '1280px'
+    // },
 
     container: {
       center: true,
